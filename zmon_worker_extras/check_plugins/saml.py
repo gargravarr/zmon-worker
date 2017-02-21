2f6182077fadedfcf6ce10d9460517ebce034eb1
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2017-02-21 16:23:24 +0100

    Added check plugin for SAML

--- zmon_worker_extras/check_plugins/saml.py
+++ zmon_worker_extras/check_plugins/saml.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import codecs
 import requests
 import os
 
+from bs4 import BeautifulSoup
 from zmon_worker_monitor.zmon_worker.errors import CheckError
 from zmon_worker_monitor.adapters.ifunctionfactory_plugin import IFunctionFactoryPlugin, propartial
+
 
 class SAMLFactory(IFunctionFactoryPlugin):
     def __init__(self):
@@ -16,9 +19,9 @@ class SAMLFactory(IFunctionFactoryPlugin):
         Called after plugin is loaded to pass the [configuration] section in their plugin info file
         :param conf: configuration dictionary
         """
-        self.saml_url = config.get('url')
-        self.saml_username = config.get('username')
-        self.saml_url = config.get('url')
+        self.saml_url = conf.get('url')
+        self.username = conf.get('username')
+        self.__password = conf.get('password')
         return
 
     def create(self, factory_ctx):
@@ -27,22 +30,24 @@ class SAMLFactory(IFunctionFactoryPlugin):
         :param factory_ctx: (dict) names available for Function instantiation
         :return: an object that implements a check function
         """
-        return propartial(SAMLWrapper, url=self.saml_url)
+        return propartial(SAMLWrapper, url=self.saml_url, user=self.username, password=self.__password)
 
-class OpenAMError(CheckError):
+
+class SAMLError(CheckError):
     def __init__(self, message):
         self.message = message
-        super(SnmpError, self).__init__()
+        super(SAMLError, self).__init__()
 
     def __str__(self):
-        return 'OpenAM Error. Message: {}'.format(self.message)
+        return 'SAML Error. Message: {}'.format(self.message)
 
 
-class OpenAMWrapper(object):
+class SAMLWrapper(object):
     def __init__(
             self,
             url,
-            path=None,
+            user=None,
+            password=None,
             params=None,
             timeout=10,
             max_retries=0,
@@ -52,25 +57,57 @@ class OpenAMWrapper(object):
         self.url = url
         self.params = params or {}
         self.headers = headers or {}
-        self.path = path
+        self.username = user
+        self.__password = password
 
-    def auth(self, chain=None, realm=None):
-        user = os.getenv("OPENAM_USER")
-        password = os.getenv("OPENAM_PASSWORD")
-        if realm:
-            self.params.update({"realm": realm})
-        if chain:
-            self.params.update({"service": chain})
-            self.params.update({"authIndexType": "service"})
-            self.params.update({"authIndexValue": chain})
-        self.headers.update({"X-OpenAM-Username": user, "X-OpenAM-Password": password, "Content-Type": "application/json"})
-        r = requests.post(self.url + "/json/authenticate", params=self.params, headers=self.headers, json={}, allow_redirects=False)
-        data = r.json()
+    def auth(self, user=None, password=None):
+        """Authenticate against the provided SAML Identity Provider."""
+
+        if not user:
+            user = self.username
+        if not password:
+            password = self.__password
+
+        session = requests.Session()
+
+        try:
+            r = session.get(self.url)
+            # NOTE: parameters are hardcoded for Shibboleth IDP
+            data = {'j_username': user,
+                    'j_password': password,
+                    'submit': 'Login'}
+
+            r = session.post(r.url, data=data)
+        except Exception, e:
+            raise SAMLError("failed to call SAML: " + str(e))
+
+        saml_xml = get_saml_response(r.text)
+        if not saml_xml:
+            raise SAMLError("Invalid SAML response.")
+
         result = {}
-        result["success"] = 'tokenId' in data
+        result["success"] = saml_xml is not None
         result["duration"] = r.elapsed.total_seconds()
         result["code"] = r.status_code
         return result
+
+
+def get_saml_response(html):
+    """
+    Parse SAMLResponse from Shibboleth page
+
+    >>> get_saml_response('<input name="a"/>')
+
+    >>> get_saml_response('<body xmlns="bla"><form><input name="SAMLResponse" value="eG1s"/></form></body>')
+    'xml'
+    """
+    soup = BeautifulSoup(html, "html.parser")
+
+    for elem in soup.find_all('input', attrs={'name': 'SAMLResponse'}):
+        saml_base64 = elem.get('value')
+        xml = codecs.decode(saml_base64.encode('ascii'), 'base64').decode('utf-8')
+        return xml
+
 
 if __name__ == '__main__':
     import sys
@@ -83,10 +120,8 @@ if __name__ == '__main__':
     plugin_manager.init_plugin_manager()
     plugin_manager.collect_plugins(load_builtins=True, load_env=True)
 
-    openam_url = sys.argv[1]
+    saml_url = sys.argv[1]
     factory_ctx = {}
 
-    # eventlog = EventLogWrapper()
-    openam = OpenAMWrapper(openam_url).auth(realm="/employees", chain="EmployeeChain")
-    print(openam)
-     
+    saml = SAMLWrapper(saml_url, user=os.getenv("SAML_USERNAME"), password=os.getenv("SAML_PASSWORD")).auth()
+    print(saml)
